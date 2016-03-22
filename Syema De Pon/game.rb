85690c57f36c50d5b18d4f9e8df7c4b5e80f8c2e
ericmargules <eric.i.margules@gmail.com>
2016-03-21 22:21:20 -0500

    fixed restart bug

--- Syema De Pon/game.rb
+++ Syema De Pon/game.rb
@@ -3,13 +3,14 @@ require_relative "timer.rb"
 
 class Game
 
-	attr_accessor :game_state, :cursor_coords, :cursor_orientation, :score, :words_matched, :pressure, :pause, :game_timer
+	attr_accessor :game_state, :cursor_coords, :cursor_orientation, :last, :score, :words_matched, :pressure, :pause, :game_timer
 
 	def initialize(cursor_coords, cursor_orientation, window)
 		@cursor_coords = cursor_coords
 		@cursor_orientation = cursor_orientation
 		@game_state = []
 		@pressure = false
+		@last = 0
 		@score = 0
 		@words_matched = []
 		@pause = false
